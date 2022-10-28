6c47b32631791cf3c73af51a94388ced5374432a
Mario Trachsel <mario@anonymerbaer.com>
2022-10-28 11:24:47 +0200

    added intro animation

--- js/scroll-behaviour.js
+++ js/scroll-behaviour.js
@@ -1,38 +1,67 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
+    //Adding the EventListener to detect scrolling
+    window.addEventListener('scroll', fadeout);
+    
+    // Function to be executed if page is scrolled
+    function fadeout() {
+        let body = document.body;
+        
+        if (body.classList.contains('home') == true){
+            setTimeout( function() {
+                setTimeout( function() {
+
+                    let title = document.getElementById("fadeout");
+                    title.classList.add("remove-title");
+
+                    let birbaum = document.getElementById("birbaum");
+                    let architektur = document.getElementById("architektur");
+                    birbaum.classList.add("translateDown");
+                    architektur.classList.add("translateUp");
+                    
+
+                }, 200);
+            }, 200 );
+
+
+
+           // console.log("body contains the class 'home'");
+        } 
+        
+        
+
+
+
+
+
+        //let fadeouts = document.querySelectorAll ('.fadeout');
+
+
+    }
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    
+
     function mouseOver() {
-        console.log("mouse is over container");
-        
-        
-        
+        console.log("mouse is over container"); 
     }
 
     function mouseOut() {
         console.log("mouse is off");
     }
 
-    function scrollMF() {
-        var y = this.deltaY;
-
-        if (y > 0) {
-            this.scrollRight = 500;
-        } else {
-            this.scrollLeft = -500;
-        }
-    }
-
     let container = document.getElementById("ct");
     container.addEventListener("mouseover", mouseOver);
     container.addEventListener("mouseout", mouseOut);
-    container.addEventListener("scroll", scrollMF);
-
-
-   /* function transformScroll() {
-        container.scrollLeft += container.deltaY + container.scrollTop;
-        container.preventDefault();
-    }
     
-    let container = document.getElementsByClassName("container-top");
-    container.onmouseover = transformScroll();  */
 })
